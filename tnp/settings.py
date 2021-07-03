acda7c4254ad8c13424d3b3a5b907b8d5bc15947
Jay Shah <sjay5200@gmail.com>
2021-07-03 22:49:09 +0530

    debugtrue

--- tnp/settings.py
+++ tnp/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--so0&05&%8jxdxn&kh+ep9zg%rak7dy^yw*1tzyoot#&$apc5g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['tnpsvnitapi.herokuapp.com', '127.0.0.1']
 CORS_ALLOWED_ORIGINS = [
