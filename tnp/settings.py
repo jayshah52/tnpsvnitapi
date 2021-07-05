897e47b5740960734dc9eaf2552dee54840a4517
Jay Shah <sjay5200@gmail.com>
2021-07-05 15:43:09 +0530

    Postgres integration

--- tnp/settings.py
+++ tnp/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import dj_database_url
 from distutils.command.config import config
 from pathlib import Path
 from tnp.config.config import username, password
@@ -126,6 +127,9 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
